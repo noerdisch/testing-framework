39b71de56ae480ced46bcf36b9d2c03d8d7c86d5
Markus Günther <markus.guenther@noerdisch.de>
2017-10-19 15:04:16 +0200

    [TASK] Downgrade phpunit to version 4.8 to be able to run the tests

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -29,7 +29,6 @@ return array(
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
     'Facebook\\WebDriver\\' => array($vendorDir . '/facebook/webdriver/lib'),
     'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
-    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'Codeception\\Extension\\' => array($vendorDir . '/codeception/codeception/ext'),
     'Codeception\\' => array($vendorDir . '/codeception/codeception/src/Codeception'),
 );
