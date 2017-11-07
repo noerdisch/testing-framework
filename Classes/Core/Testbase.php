1dd66531a1f966d599dc68cfa80acb4e2ac5e87c
Markus Günther <markus.guenther@noerdisch.de>
2017-11-07 15:25:54 +0100

    [FEATURE] Define more base constants and load default configuration

--- Classes/Core/Testbase.php
+++ Classes/Core/Testbase.php
@@ -16,6 +16,7 @@ namespace Noerdisch\TestingFramework\Core;
  */
 
 use Codeception\Util\Autoload;
+use TYPO3\CMS\Core\Configuration\ConfigurationManager;
 use TYPO3\CMS\Core\Core\Bootstrap;
 use TYPO3\CMS\Core\Core\ClassLoadingInformation;
 use TYPO3\CMS\Core\Utility\ArrayUtility;
@@ -98,6 +99,31 @@ class Testbase
         defined('SUB') ?: define('SUB', chr(26));
         defined('CRLF') ?: define('CRLF', CR . LF);
 
+        // Security related constant: Default value of fileDenyPattern
+        define('FILE_DENY_PATTERN_DEFAULT', '\\.(php[3-7]?|phpsh|phtml|pht)(\\..*)?$|^\\.htaccess$');
+        // Security related constant: List of file extensions that should be registered as php script file extensions
+        define('PHP_EXTENSIONS_DEFAULT', 'php,php3,php4,php5,php6,php7,phpsh,inc,phtml,pht');
+
+        // Service error constants
+        // General error - something went wrong
+        define('T3_ERR_SV_GENERAL', -1);
+        // During execution it showed that the service is not available and should be ignored. The service itself should call $this->setNonAvailable()
+        define('T3_ERR_SV_NOT_AVAIL', -2);
+        // Passed subtype is not possible with this service
+        define('T3_ERR_SV_WRONG_SUBTYPE', -3);
+        // Passed subtype is not possible with this service
+        define('T3_ERR_SV_NO_INPUT', -4);
+        // File not found which the service should process
+        define('T3_ERR_SV_FILE_NOT_FOUND', -20);
+        // File not readable
+        define('T3_ERR_SV_FILE_READ', -21);
+        // File not writable
+        define('T3_ERR_SV_FILE_WRITE', -22);
+        // Passed subtype is not possible with this service
+        define('T3_ERR_SV_PROG_NOT_FOUND', -40);
+        // Passed subtype is not possible with this service
+        define('T3_ERR_SV_PROG_FAILED', -41);
+
         if (!defined('TYPO3_OS')) {
             // Operating system identifier
             // Either "WIN" or empty string
@@ -131,6 +157,9 @@ class Testbase
     {
         define('PATH_site', $this->getWebRoot());
         define('PATH_thisScript', PATH_site . 'typo3/cli_dispatch.phpsh');
+        define('TYPO3_mainDir', 'typo3/');
+        define('PATH_typo3', PATH_site . TYPO3_mainDir);
+        define('PATH_typo3conf', PATH_site . 'typo3conf/');
         $_SERVER['SCRIPT_NAME'] = PATH_thisScript;
 
         if (!file_exists(PATH_thisScript)) {
@@ -546,6 +575,20 @@ class Testbase
 
         $classLoader = require $autoloadFile;
         Bootstrap::getInstance()->initializeClassLoader($classLoader);
+    }
+
+    /**
+     * Provides the default configuration in $GLOBALS['TYPO3_CONF_VARS'].
+     *
+     * @return void
+     */
+    public function initializeDefaultConfiguration()
+    {
+        $configurationManager = new ConfigurationManager();
+        $GLOBALS['TYPO3_CONF_VARS'] = $configurationManager->getDefaultConfiguration();
+
+        // avoid failing tests that rely on HTTP_HOST retrieval
+        $GLOBALS['TYPO3_CONF_VARS']['trustedHostsPattern'] = '.*';
     }
 
     /**
