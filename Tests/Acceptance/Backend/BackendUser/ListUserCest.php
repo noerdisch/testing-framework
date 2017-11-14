c2c1805a454b8b7ffeda2e69a00466380421e906
Markus Günther <markus.guenther@noerdisch.de>
2017-11-14 13:28:20 +0100

    [TASK] Make ListUserCest 7.6 compatible

--- Tests/Acceptance/Backend/BackendUser/ListUserCest.php
+++ Tests/Acceptance/Backend/BackendUser/ListUserCest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace TYPO3\CMS\Core\Tests\Acceptance\Backend\BackendUser;
 
 /*
@@ -15,6 +16,7 @@ namespace TYPO3\CMS\Core\Tests\Acceptance\Backend\BackendUser;
  */
 
 use Noerdisch\TestingFramework\Core\Acceptance\Step\Backend\Admin;
+use Codeception\Util\Locator;
 
 /**
  * List User tests
@@ -26,17 +28,17 @@ class ListUserCest
      */
     public function _before(Admin $I)
     {
-        $I->useExistingSession();
+        $I->login();
         // Ensure main content frame is fully loaded, otherwise there are load-race-conditions
-        $I->switchToIFrame('list_frame');
-        $I->waitForText('Web Content Management System');
-        $I->switchToIFrame();
 
+        $I->switchToIFrame();
+        $I->waitForElementVisible('#typo3-menu');
         $I->see('Backend users');
         $I->click('Backend users');
 
         // switch to content iframe
-        $I->switchToIFrame('list_frame');
+        $I->switchToIFrame('content');
+        $I->waitForText('Backend User Listing');
     }
 
     /**
@@ -47,7 +49,7 @@ class ListUserCest
         $I->see('Backend User Listing');
 
         $I->wantTo('See the table of users');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact four Backend Users created from the Fixtures
         $this->checkCountOfUsers($I, 4);
@@ -59,14 +61,14 @@ class ListUserCest
     public function filterUsersByUsername(Admin $I)
     {
         $I->wantTo('See the table of users');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
         // We expect exact four Backend Users created from the Fixtures
-        $I->canSeeNumberOfElements('#typo3-backend-user-list tbody tr', 4);
+        $I->canSeeNumberOfElements('table tbody tr', $this->getCountOfUsers(4));
 
         $I->wantTo('Filter the list of user by valid username admin');
         $I->fillField('#tx_Beuser_username', 'admin');
         $I->click('Filter');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact one fitting Backend User created from the Fixtures
         $this->checkCountOfUsers($I, 1);
@@ -74,7 +76,7 @@ class ListUserCest
         $I->wantTo('Filter the list of user by valid username administrator');
         $I->fillField('#tx_Beuser_username', 'administrator');
         $I->click('Filter');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact no fitting Backend User created from the Fixtures
         $this->checkCountOfUsers($I, 0);
@@ -86,14 +88,14 @@ class ListUserCest
     public function filterUsersByAdmin(Admin $I)
     {
         $I->wantTo('See the table of users');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
         // We expect exact four Backend Users created from the Fixtures
-        $I->canSeeNumberOfElements('#typo3-backend-user-list tbody tr', 4);
+        $I->canSeeNumberOfElements('table.table-striped  tbody tr', $this->getCountOfUsers(4));
 
         $I->wantToTest('Filter BackendUser and see only admins');
         $I->selectOption('#tx_Beuser_usertype', 'Admin only');
         $I->click('Filter');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact two fitting Backend Users created from the Fixtures
         $this->checkCountOfUsers($I, 2);
@@ -101,7 +103,7 @@ class ListUserCest
         $I->wantToTest('Filter BackendUser and see normal users');
         $I->selectOption('#tx_Beuser_usertype', 'Normal users only');
         $I->click('Filter');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact two fitting Backend Users created from the Fixtures
         $this->checkCountOfUsers($I, 2);
@@ -113,14 +115,14 @@ class ListUserCest
     public function filterUsersByStatus(Admin $I)
     {
         $I->wantTo('See the table of users');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
         // We expect exact four Backend Users created from the Fixtures
-        $I->canSeeNumberOfElements('#typo3-backend-user-list tbody tr', 4);
+        $I->canSeeNumberOfElements('table.table-striped tbody tr', $this->getCountOfUsers(4));
 
         $I->wantToTest('Filter BackendUser and see only active users');
         $I->selectOption('#tx_Beuser_status', 'Active only');
         $I->click('Filter');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact two fitting Backend Users created from the Fixtures
         $this->checkCountOfUsers($I, 2);
@@ -128,7 +130,7 @@ class ListUserCest
         $I->wantToTest('Filter BackendUser and see only inactive users');
         $I->selectOption('#tx_Beuser_status', 'Inactive only');
         $I->click('Filter');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact two fitting Backend Users created from the Fixtures
         $this->checkCountOfUsers($I, 2);
@@ -140,14 +142,14 @@ class ListUserCest
     public function filterUsersByLogin(Admin $I)
     {
         $I->wantTo('See the table of users');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
         // We expect exact four Backend Users created from the Fixtures
-        $I->canSeeNumberOfElements('#typo3-backend-user-list tbody tr', 4);
+        $I->canSeeNumberOfElements('table.table-striped tbody tr', $this->getCountOfUsers(4));
 
         $I->wantToTest('Filter BackendUser and see only users logged in before');
         $I->selectOption('#tx_Beuser_logins', 'Logged in before');
         $I->click('Filter');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact two fitting Backend Users created from the Fixtures
         $this->checkCountOfUsers($I, 2);
@@ -155,7 +157,7 @@ class ListUserCest
         $I->wantToTest('Filter BackendUser and see only users never logged in before');
         $I->selectOption('#tx_Beuser_logins', 'Never logged in');
         $I->click('Filter');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact two fitting Backend Users created from the Fixtures
         $this->checkCountOfUsers($I, 2);
@@ -167,15 +169,15 @@ class ListUserCest
     public function filterUsersByUserGroup(Admin $I)
     {
         $I->wantTo('See the table of users');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
         // We expect exact four Backend Users created from the Fixtures
-        $I->canSeeNumberOfElements('#typo3-backend-user-list tbody tr', 4);
+        $I->canSeeNumberOfElements('table.table-striped  tbody tr', $this->getCountOfUsers(4));
 
         // We expect exact one Backend Users created from the Fixtures has the usergroup named 'editor-group'
         $I->wantToTest('Filter BackendUser and see only users with given usergroup');
         $I->selectOption('#tx_beuser_backendUserGroup', 'editor-group');
         $I->click('Filter');
-        $I->waitForElementVisible('#typo3-backend-user-list');
+        $I->waitForElementVisible('table.table-striped');
 
         // We expect exact one fitting Backend User created from the Fixtures
         $this->checkCountOfUsers($I, 1);
@@ -187,9 +189,21 @@ class ListUserCest
      */
     protected function checkCountOfUsers(Admin $I, $countOfUsers)
     {
-        $I->canSeeNumberOfElements('#typo3-backend-user-list tbody tr', $countOfUsers);
+        $I->canSeeNumberOfElements('table tbody tr', $this->getCountOfUsers($countOfUsers));
         $I->wantToTest('If a number of users is shown in the footer row');
-        $I->canSeeNumberOfElements('#typo3-backend-user-list tfoot tr', 1);
-        $I->see($countOfUsers . ' Users', '#typo3-backend-user-list tfoot tr');
+        $I->canSeeNumberOfElements(Locator::lastElement('table tbody tr'), 1);
+        $I->see($countOfUsers . ' Users', Locator::lastElement('table tbody tr'));
+    }
+
+    /**
+     * We need to increment the number of users because the number of tr elements are including a footer.
+     * In TYPO3 version 8 and higher the markup has been changed so that the footer row is a separate tfoot row.
+     *
+     * @param int $amount
+     * @return int
+     */
+    protected function getCountOfUsers($amount)
+    {
+        return $amount + 1;
     }
 }
