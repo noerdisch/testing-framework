ef2d0c28c2743468b4512ff8b8da2ddabdb33e9f
Markus Günther <markus.guenther@noerdisch.de>
2017-11-15 06:51:47 +0100

    [TASK] Adjust CategoryTreeCest to version 7.6

--- Tests/Acceptance/Backend/Formhandler/CategoryTreeCest.php
+++ Tests/Acceptance/Backend/Formhandler/CategoryTreeCest.php
@@ -14,6 +14,7 @@ namespace TYPO3\CMS\Core\Tests\Acceptance\Backend\Formhandler;
  * The TYPO3 project - inspiring people to share!
  */
 
+use Codeception\Util\Locator;
 use Noerdisch\TestingFramework\Core\Acceptance\Step\Backend\Admin;
 
 /**
@@ -26,10 +27,18 @@ class CategoryTreeCest
      */
     public function _before(Admin $I)
     {
-        $I->useExistingSession();
+        $I->login();
         // Ensure main content frame is fully loaded, otherwise there are load-race-conditions
-        $I->switchToIFrame('list_frame');
-        $I->waitForText('Web Content Management System');
+        $I->switchToIFrame('content');
+        $I->waitForText('New TYPO3 site');
+        $I->switchToIFrame();
+    }
+
+    /**
+     * @param Admin $I
+     */
+    public function _after(Admin $I)
+    {
         $I->switchToIFrame();
     }
 
@@ -39,9 +48,9 @@ class CategoryTreeCest
     public function checkIfCategoryListIsAvailable(Admin $I)
     {
         // A sub-element of web module is show
-        $I->waitForElementVisible('#web .modulemenu-group-container .modulemenu-item');
+        $I->waitForElementVisible('#web .typo3-module-menu-group-container .typo3-module-menu-item');
         $I->click('#web_list');
-        $I->switchToIFrame('list_frame');
+        $I->switchToIFrame('content');
         $I->waitForElement('#recordlist-sys_category');
         $I->seeNumberOfElements('#recordlist-sys_category table > tbody > tr', [5, 100]);
     }
@@ -52,23 +61,25 @@ class CategoryTreeCest
     public function editCategoryItem(Admin $I)
     {
         // A sub-element of web module is show
-        $I->waitForElementVisible('#web .modulemenu-group-container .modulemenu-item');
+        $I->waitForElementVisible('#web .typo3-module-menu-group-container .typo3-module-menu-item');
         $I->click('#web_list');
-        $I->switchToIFrame('list_frame');
+        $I->switchToIFrame('content');
         // Collapse all tables and expand category again - ensures category fits into window
-        $I->executeJS('$(\'.icon-actions-view-list-collapse\').click();');
+        $I->click('.icon-actions-view-list-collapse');
+        //$I->executeJS('$(\'.icon-actions-view-list-collapse\').click();');
         $I->wait(1);
-        $I->executeJS('$(\'a[data-table="sys_category"] .icon-actions-view-list-expand\').click();');
+        $I->click('.icon-actions-view-list-expand');
+        //$I->executeJS('$(\'a[data-table="sys_category"] .icon-actions-view-list-expand\').click();');
         $I->wait(1);
         // Select category with id 7
         $I->click('#recordlist-sys_category tr[data-uid="7"] a[data-original-title="Edit record"]');
         // Change title and level to root
         $I->fillField('input[data-formengine-input-name="data[sys_category][7][title]"]', 'level-1-4');
-        $I->click('.identifier-7 text');
-        $I->click('.identifier-3 text');
+        $I->click('//*[@id="ext-gen7"]/div/li/ul/li[1]/ul/li/ul/li[1]/div/input');
+        $I->click('//*[@id="ext-gen7"]/div/li/ul/li[1]/ul/li/ul/li[3]/div/input');
         $I->click('button[name="_savedok"]');
         // Wait for tree and check if isset level-1-4
-        $I->waitForElement('.svg-tree-wrapper svg');
-        $I->see('level-1-4', '.svg-tree-wrapper svg .node text');
+        $I->waitForElement('.form-section .x-tree-root-ct');
+        $I->see('level-1-4', '.form-section .x-tree-root-ct span');
     }
 }
